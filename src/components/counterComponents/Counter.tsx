f7eb2aa5192a1eb9e7a27b9faa46d34b1ba71180
Michael Dudzik <mkdudik15@gmail.com>
2025-01-12 18:10:08 +0300

    adding logic of disabling set and inc buttons

--- src/components/counterComponents/Counter.tsx
+++ src/components/counterComponents/Counter.tsx
@@ -5,19 +5,37 @@ import {Button} from "../Button";
 import {Display} from "../counterComponents/Display";
 
 type CounterPropsType = {
-    value:number
-    maxValue:number
-    incrementFunc:()=>void
-    resetFunc:()=>void
-    isResetDisabled:boolean
-    isIncDisabled:boolean
+    value: number
+    maxValue: number
+    incrementFunc: () => void
+    resetFunc: () => void
+    isResetDisabled: boolean
+    isIncDisabled: boolean
+    isSet: boolean
+    startValue:number
+    isError: boolean
 }
-export const Counter: React.FC<CounterPropsType>= ({value,maxValue,incrementFunc,resetFunc,isResetDisabled,isIncDisabled}) => {
+export const Counter: React.FC<CounterPropsType> = ({
+                                                        value,
+                                                        maxValue,
+                                                        incrementFunc,
+                                                        resetFunc,
+                                                        isResetDisabled,
+                                                        isIncDisabled,
+                                                        isSet,
+                                                        startValue,
+                                                        isError
+                                                    }) => {
 
-    const resultValue=value === maxValue
+    const resultValue = value === maxValue
     return (
         <div className='CounterStyled'>
-            <Display value={value} isMaxValue={resultValue}/>
+            <Display value={value}
+                     isMaxValue={resultValue}
+                     isSet={isSet}
+                     startValue={startValue}
+                     isError={isError}
+            />
             <div className='wrapper'>
                 <Button className='btn' onClick={incrementFunc} disabled={isIncDisabled}>INC</Button>
                 <Button className='btn' onClick={resetFunc} disabled={isResetDisabled}>RES</Button>
--- src/components/setCounterComponents/SetCounter.tsx
+++ src/components/setCounterComponents/SetCounter.tsx
@@ -1,19 +1,27 @@
-import React, {useEffect, useState} from 'react';
-import '../styles/style.css'
-
-import {InputComponent} from "./InputComponent";
-
-import {Button} from "../Button";
+import React from 'react';
+import '../styles/style.css';
+import { InputComponent } from "./InputComponent";
+import { Button } from "../Button";
 
 type SetCounterProps = {
-    startValue: number
-    maxValue: number
-    setStartValue: (startValue:number)=>void
-    setMaxValue:( maxValue: number)=>void
-    onClickValue:()=>void
-}
-export const SetCounter: React.FC<SetCounterProps> = ({startValue,maxValue,setStartValue,setMaxValue,onClickValue}) => {
+    startValue: number;
+    maxValue: number;
+    setStartValue: (startValue: number) => void;
+    setMaxValue: (maxValue: number) => void;
+    onClickValue: () => void;
+    isSet: boolean;
+    isError: boolean;
+};
 
+export const SetCounter: React.FC<SetCounterProps> = ({
+                                                          startValue,
+                                                          maxValue,
+                                                          setStartValue,
+                                                          setMaxValue,
+                                                          onClickValue,
+                                                          isSet,
+                                                          isError
+                                                      }) => {
     return (
         <div className="CounterStyled">
             <div className='InputContainer'>
@@ -21,22 +29,20 @@ export const SetCounter: React.FC<SetCounterProps> = ({startValue,maxValue,setSt
                     label='max value'
                     value={maxValue}
                     onChange={setMaxValue}
-                    className= 'InputStyled'
+                    className='InputStyled'
+                    isError={isError}
                 />
                 <InputComponent
                     label='start value'
                     value={startValue}
                     onChange={setStartValue}
-                    className= 'InputStyled'
+                    className='InputStyled'
+                    isError={isError}
                 />
             </div>
             <div className='wrapper'>
-                <Button className='btn' onClick={()=>onClickValue()} disabled={false}>set</Button>
+                <Button className='btn' onClick={onClickValue} disabled={isSet || isError}>set</Button>
             </div>
-
-
         </div>
     );
 };
-
-
